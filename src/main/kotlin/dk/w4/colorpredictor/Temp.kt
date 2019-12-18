e69c7af24b8b360256c7537cc90dbf0a816619a2
Steffen Mogensen <feber.sm@gmail.com>
2019-12-18 19:03:51 +0100

    fixed compiler error

--- src/main/kotlin/dk/w4/colorpredictor/Temp.kt
+++ src/main/kotlin/dk/w4/colorpredictor/Temp.kt
@@ -9,7 +9,8 @@ fun main() {
     val file = File("/Users/silverbaq/Github/color-predictor/data/image/test/blue/8007-home.jpg")
     val image = ImageIO.read(file)
     val target = predector.getTargetModelData("my-image", image)
+    val model = predector.loadModel("/Users/silverbaq/Github/color-predictor/data/image/model.json")
 
-    val result = predector.PredictColor(target, 3)
+    val result = predector.PredictColor(target, model, 3)
     println(result) // blue
 }
